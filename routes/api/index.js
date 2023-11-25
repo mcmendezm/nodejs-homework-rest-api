1c29336689268ad966d592bc21460129ced86929
Camila Mendez <mcmendezmanotas@gmail.com>
2023-11-25 11:26:58 -0500

    eliminando ruta de avatars en el index

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const contactRoutes = require('./contacts');
 const userRoutes = require('./users');
 const router = express.Router();
-const avatarRouter = require('../avatar'); 
+
 
 // Rutas para contactos
 router.use('/contacts', contactRoutes);
@@ -10,7 +10,6 @@ router.use('/contacts', contactRoutes);
 // Rutas para usuarios
 router.use('/users', userRoutes);
 
-// Ruta para avatars
-router.use('/users/avatars', avatarRouter); 
+
 
 module.exports = router;
